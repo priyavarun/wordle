5a758f92d92e3b826a1e3ba0fe8c837cefedc900
Priya <priyyavarun@gmail.com>
2022-09-03 16:12:44 +0530

    Initial wordle commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [solution, setSolution] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((data) =>
+        setSolution(data[Math.floor(Math.random() * data.length)].word)
+      );
+  }, [setSolution]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Wordle App</h1>
+      {solution && <span>Solution is {solution}</span>}
+    </>
   );
 }
 
